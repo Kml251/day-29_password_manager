930eadbe7999714a04827efd69d58a12df6e2ebe
Kml251 <2024455@student.cct.ie>
2026-02-02 16:00:00 +0000

    adding commend

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ def generate_password():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    # Use list comprehension instead of looping for
     password_list =[choice(letters) for _ in range(randint(8, 10)) ]+ \
                     [choice(symbols) for _ in range(randint(2, 4))] + \
                     [choice(numbers) for _ in range(randint(2, 4))]
     shuffle(password_list)
 
+    # Get the password as a value
     password ="".join(password_list)
+    # Add the generated password to password field
     password_entry.insert(0, password)
-    print(f"Your password is: {password}")
+    # print(f"Your password is: {password}")
     pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -25,9 +28,11 @@ def save_password():
     mail = mail_entry.get()
     password = password_entry.get()
 
+    # Check if all required field filled
     if website == "" or mail == "" or password == "":
         messagebox.showerror("Oopss!", "Please enter all required information")
     else:
+        # Optional: after generated password, user could check if all info is correct
         is_ok = messagebox.askyesno(title=website, message=f"These are the details entered: \nEmail: {mail}"
                                                            f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
@@ -40,10 +45,12 @@ def save_password():
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+# Create a window with Tkinter module
 window = Tk()
 window.title("Password Generator")
-window.config(padx=50, pady=50)
+window.config(padx=40, pady=40)
 
+# Use the image as a graphic
 canvas = Canvas(width=200, height=200)
 key_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=key_img)
@@ -58,21 +65,20 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Entry
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=40)
+website_entry.grid(column=1, row=1, columnspan=3)
 website_entry.focus()
-mail_entry = Entry(width=35)
-mail_entry.grid(column=1, row=2, columnspan=2)
-mail_entry.insert(0, "angela@mail.com")
+mail_entry = Entry(width=40)
+mail_entry.grid(column=1, row=2, columnspan=3)
+# Shown a text on the entry field.
+mail_entry.insert(0, "user@mail.com")
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3)
 
 # Button
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
-add_button = Button(text="Add", width=36, command=save_password)
-add_button.grid(column=1, row=4, columnspan=2)
-
-
+add_button = Button(text="Add", width=41, command=save_password)
+add_button.grid(column=1, row=4, columnspan=3)
 
 window.mainloop()
